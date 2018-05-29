27ada962bd020360187193729e6eb278eadcd329
Glitch (fcc-iimagesearch) <none>
2018-05-29 03:43:04 +0000

    😼🐄 Updated with Glitch

---
+++ searcher.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let https=require('https')
+
+// Replace the subscriptionKey string value with your valid subscription key.
+let subscriptionKey = process.env.IMAGEKEY
+
+
+exports.doSearch= function(term,offset) {
+   var results=[]
+    // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+    // search APIs.  In the future, regional endpoints may be available.  If you
+    // encounter unexpected authorization errors, double-check this host against
+    // the endpoint for your Bing Search instance in your Azure dashboard.
+    let host = 'api.cognitive.microsoft.com';
+    let path = '/bing/v7.0/images/search';
+    
+    let response_handler = function (response) {
+        let body = '';
+        response.on('data', function (d) {
+            body += d;
+        });
+        response.on('end', function () {
+            console.log('\nRelevant Headers:\n');
+            for (var header in response.headers)
+                // header keys are lower-cased by Node.js
+                if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
+                     console.log(header + ": " + response.headers[header]);
+            var rawResults = JSON.parse(body)
+            
+            for (var c=0;c<rawResults.length;c++) {
+              var newResult={url:rawResults[c].contentUrl,
+                            snippet:rawResults[c].name,
+                            thumbnail:rawResults[c].thumbnailUrl,
+                            context:rawResults[c].hostPageUrl}
+              results.push(newResult)
+            }
+        console.log(results)
+          return results
+        });
+        response.on('error', function (e) {
+            console.log('Error: ' + e.message);
+        });
+    };
+
+    let bing_image_search = function (search) {
+      console.log('Searching images for: ' + term);
+      let request_params = {
+            method : 'GET',
+            hostname : host,
+            path : path + '?q=' + encodeURIComponent(search)+
+        '&offset='+offset,
+            headers : {
+                'Ocp-Apim-Subscription-Key' : subscriptionKey,
+            }
+        };
+
+        let req = https.request(request_params, response_handler);
+        req.end();
+    }
+
+    if (subscriptionKey.length === 32) {
+        bing_image_search(term,offset);
+    } else {
+        console.log('Invalid Bing Search API subscription key!');
+        console.log('Please paste yours into the source code.');
+    }   
+}
+
+
--- server.js
+++ server.js
@@ -1,22 +1,84 @@
 // server.js
 // where your node app starts
 
+var collName='imagesearch'
+var urlPrefix='https://fcc-iimagesearch.glitch.me/'
+var dbName = 'fcc-iimagesearch'
+var fnUrlNum='urlNum'
+var fnUrl='url'
+
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var mongo=require('mongodb').MongoClient
+var searcher =require('./searcher')
+var recLimit = 10
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
+
+
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.get("/api/imagesearch/:term", function (request, response) {
+  var url = process.env.MONGODB_URL
+  console.log('Search')
+  var req = request.url
+  var doTerm = decodeURI(request.params.term)
+  var doOffset = request.query.offset
+  console.log(doTerm)
+  console.log(doOffset)
+  
+  mongo.connect(url,{useNewUrlParser:true},function(err,db) {
+    if (err) {response.send(JSON.stringify(err))} 
+
+    var dbo=db.db(dbName)
+    var coll = dbo.collection(collName)
+    
+    var newDocObj = {"term":doTerm,"when":(new Date).toISOString()}
+    var newUrl =""
+    coll.insert(newDocObj,function(err,data) {
+    if (err) {response.send(JSON.stringify(err))} 
+
+    db.close()
+    
+    var searchResults = searcher.doSearch(doTerm,doOffset)
+    
+    response.send(JSON.stringify(searchResults))
+    })
+
+})
+})
+
+
+app.get("/api/latest/imagesearch",function (request,response) {
+  
+  var url = process.env.MONGODB_URL  
+  
+  mongo.connect(url,{useNewUrlParser:true},function(err,db) {
+    if (err) { response.send(JSON.stringify(err)) } 
+    // get url associated with link
+    var dbo=db.db(dbName)
+    var coll = dbo.collection(collName)
+    
+    coll.find({},{fields:{_id:0}}).sort({_id:-1}).limit(recLimit).toArray(function(err,docs){
+          if (err) {response.send(JSON.stringify(err))}
+          response.send(JSON.stringify(docs))
+          db.close()
+    
+        })  
+    })
+  
+})
+
+app.get('/',function(request,response) {
+  response.send('')
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-});
+})
